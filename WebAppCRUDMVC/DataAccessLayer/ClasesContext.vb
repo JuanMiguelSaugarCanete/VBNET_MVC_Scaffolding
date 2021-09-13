38170b9cbd3144e114225218adbb2d7d980b010a
JuanMiguelSaugarCanete <juanmiguelsaugarcanete@gmail.com>
2021-09-13 14:02:58 +0200

    ContextCreado

---
+++ WebAppCRUDMVC/DataAccessLayer/ClasesContext.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.Entity
+Imports System.Data.Entity.ModelConfiguration.Conventions
+
+Public Class ClasesContext
+    Inherits DbContext
+    Public Sub New()
+        MyBase.New("ClasesContext")
+    End Sub
+    Protected Overrides Sub OnModelCreating(ByVal modelBuilder As DbModelBuilder)
+        modelBuilder.Conventions.Remove(Of PluralizingTableNameConvention)()
+    End Sub
+End Class
