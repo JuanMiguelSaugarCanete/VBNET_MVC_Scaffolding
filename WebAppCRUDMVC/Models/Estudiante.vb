7a068fda552db9b7278b42ec16554ac81f71bf2c
JuanMiguelSaugarCanete <juanmiguelsaugarcanete@gmail.com>
2021-09-13 13:58:52 +0200

    ModeloEstudiantes

---
+++ WebAppCRUDMVC/Models/Estudiante.vb
@@ -0,0 +1,17 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+
+Public Enum cursos
+    DAM
+    DAW
+    SMIR
+    ASIR
+End Enum
+Public Class Estudiante
+    <DataKeyProperty(ID)>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property ID As Integer
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property Curso As cursos
+    Public Property FechaInicio As DateTime
+End Class
